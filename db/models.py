5235e6ae20574061ba506876efe1edcb4dd5b0f2
Bianca <bianca.petrut00@e-uvt.ro>
2022-08-26 14:22:07 +0000

    26.06

--- db/models.py
+++ db/models.py
@@ -1,79 +1,108 @@
 from sqlalchemy import Table, Column, Integer, String, MetaData, Boolean, create_engine, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import as_declarative
 import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 
-engine = create_engine('sqlite:///pdatabase.db', echo = True)
-#meta = MetaData()
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pdatabase.db'
+
+engine = create_engine('sqlite:///pdatabase.db', echo=True)
+# meta = MetaData()
+db = SQLAlchemy(app)
 
 @as_declarative()
 class BaseModel(object):
 
     created_at = Column(DateTime, default=lambda: datetime.datetime.now())
     updated_at = Column(DateTime, onupdate=lambda: datetime.datetime.now())
-    def _to_dict(self):
-        _dict = {col.name: getattr(self, col.name)
-                 for col in self.__table__.columns}
-        return _dict
+
+    def __init__(self, created_at: str, updated_at: str):
+        self.created_at = created_at
+        self.updated_at = updated_at
+
+    # def _to_dict(self):
+    #     _dict = {col.name: getattr(self, col.name)
+    #              for col in self.__table__.columns}
+    #     return _dict
 
 
 class User(BaseModel):
-	__tablename__ = 'users'
-	userID = Column(Integer, primary_key=True)
-	name = Column(String)
-	isAdmin = Column(Boolean)
-	password = Column(String)
+    __tablename__ = 'users'
+    userID = Column(Integer, primary_key=True)
+    name = Column(String)
+    isAdmin = Column(Boolean, default=False)
+    password = Column(String)
+
+    def __init__(self, created_at: str, updated_at: str, userID: int, name: str, isAdmin: bool, password: str):
+        self.userID = userID
+        self.name = name
+        self.isAdmin = isAdmin
+        self.password = password
+        super(User, self).__init__()
+
+
+    def create(name, password):  # create new user
+        new_user = User(datetime.datetime.now(), datetime.datetime.now(), '2', name, False, password)
+        db.session.add(new_user)
+        db.session.commit()
+
 
 # users = Table(
-# 	'users', meta,
-# 	Column('userID', Integer, primary_key=True),
-# 	Column('name', String),
-# 	Column('isAdmin', Boolean),
-# 	Column('password', String))
+#     'users', meta,
+#     Column('userID', Integer, primary_key=True),
+#     Column('name', String),
+#     Column('isAdmin', Boolean),
+#     Column('password', String))
 
 
 class Container(BaseModel):
-	__tablename__ = 'containers'
-	containerID = Column(Integer, primary_key=True)
-	name = Column(String)
-	description = Column(String)
-	ownerID = Column(Integer, ForeignKey('users.userID'))
+    __tablename__ = 'containers'
+    containerID = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+    ownerID = Column(Integer, ForeignKey('users.userID'))
 
 # containers = Table(
-# 	'containers', meta,
-# 	Column('containerID', Integer, primary_key=True),
-# 	Column('name', String),
-# 	Column('description', String),
-# 	Column('ownerID', Integer, ForeignKey('users.userID')))
+#     'containers', meta,
+#     Column('containerID', Integer, primary_key=True),
+#     Column('name', String),
+#     Column('description', String),
+#     Column('ownerID', Integer, ForeignKey('users.userID')))
+
 
 class Object(BaseModel):
-	__tablename__ = 'objects'
-	objectID = Column(Integer, primary_key=True)
-	name = Column(String)
-	containerID = Column(Integer, ForeignKey('containers'))
-	description = Column(String)
+    __tablename__ = 'objects'
+    objectID = Column(Integer, primary_key=True)
+    name = Column(String)
+    containerID = Column(Integer, ForeignKey('containers'))
+    description = Column(String)
 
 # objects = Table(
-# 	'objects', meta,
-# 	Column('objectID', Integer, primary_key=True),
-# 	Column('name', String),
-# 	Column('containerID', Integer, ForeignKey('containers')),
-# 	Column('description', String))
+#     'objects', meta,
+#     Column('objectID', Integer, primary_key=True),
+#     Column('name', String),
+#     Column('containerID', Integer, ForeignKey('containers')),
+#     Column('description', String))
+
 
 class AuthToken(BaseModel):
-	__tablename__ = 'auth_tokens'
-	token = Column(String, primary_key=True)
-	userID = Column(Integer, ForeignKey('users'))
-	expireDate = Column(DateTime)
+    __tablename__ = 'auth_tokens'
+    token = Column(String, primary_key=True)
+    userID = Column(Integer, ForeignKey('users'))
+    expireDate = Column(DateTime)
 
 # auth_tokens = Table(
-# 	'authTokens', meta,
-# 	Column('token', String, primary_key=True),
-# 	Column('userID', Integer, ForeignKey('users')),
-# 	Column('expireDate', DateTime))
+#     'authTokens', meta,
+#     Column('token', String, primary_key=True),
+#     Column('userID', Integer, ForeignKey('users')),
+#     Column('expireDate', DateTime))
 
-def dict():
-	if isinstance(obj, BaseModel):
-		return obj._to_dict()
-#import pdb; pdb.set_trace()
+
+# def dict():
+#     if isinstance(obj, BaseModel):
+#         return obj._to_dict()
+# import pdb; pdb.set_trace()
 BaseModel.metadata.create_all(engine)
